4f0fa86e34d27a8af9d87d411581809605a5382e
Collins C Augustine <colins4chinonso@gmail.com>
2026-02-23 02:17:33 +0100

    linking help

--- contract/src/events/topics.rs
+++ contract/src/events/topics.rs
@@ -74,6 +74,7 @@ pub const ACT_VOTED: &str      = "voted";
 pub const ACT_DELEGATED: &str  = "delegated";
 pub const ACT_FINALIZED: &str  = "finalized";
 pub const ACT_PROPOSED: &str   = "proposed";
+pub const VOTED: &str      = "vote";      // A help for proper issue linking
 
 // =========== Milestone-specific actions ===========
 
