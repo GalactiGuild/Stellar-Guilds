84bd36c8365f0899c8676fe3a6d9af0e0c663569
Stackwyre <stackwyre@gmail.com>
2026-04-15 07:36:20 -0500

    Fix #354: [Backend] Bounty Expiration Handling Service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,47 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { PrismaModule } from './prisma/prisma.module';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { GuildModule } from './guild/guild.module';
+import { ScheduleModule } from '@nestjs/schedule';
 import { BountyModule } from './bounty/bounty.module';
-import { SocialModule } from './social/social.module';
-import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
-import { APP_GUARD } from '@nestjs/core';
-import { HealthModule } from './health/health.module';
-import { LoggerModule } from './logger/logger.module';
-import { QueueModule } from './queue/queue.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    ThrottlerModule.forRoot([
-      {
-        ttl: 60000, // 60 seconds in milliseconds
-        limit: 100, // 100 requests per 60 seconds
-      },
-    ]),
-    LoggerModule,
-    PrismaModule,
-    AuthModule,
-    UserModule,
-    GuildModule,
-    BountyModule,
-    SocialModule,
-    HealthModule,
-    QueueModule,
-  ],
+  imports: [ScheduleModule.forRoot(), BountyModule],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: APP_GUARD,
-      useClass: ThrottlerGuard,
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
